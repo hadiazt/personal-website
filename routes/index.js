61bdb0723ea30922cbb19dbd38ba30e48b8fef25
hadi <65118892+hadiazt@users.noreply.github.com>
2021-12-03 20:12:32 +0330

    API for saving & rendering Discord Stats

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,102 @@
 const { Router } = require("express");
-
 const router = new Router();
 
 const { Database } = require("beta.db")
-const db = new Database('./config.json');
+const db = new Database('./settings/config.json');
+const API = new Database('./settings/api.json');
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.login(db.get('BOT').TOKEN)
+
+client.on("ready", async () => {
+    console.log('CONNETED TO : ' + client.user.tag);
+
+    setInterval(() => {
+        const user = client.users.cache.get(db.get('BOT').USER)
+
+        if (user.presence.activities[0]) {
+            if (user.presence.activities[0].type === 'CUSTOM_STATUS') {
+                if (user.presence.activities[1]) {
+                    API.clear()
+                    var API_VALUE = {
+                        'INFO': {
+                            'NAME': user.username,
+                            'STATUS': user.presence.status || 'N/A',
+                            'AVATAR': user.displayAvatarURL({ size: 2048, format: 'jpg' }),
+                            'CUSTOM_STATUS': user.presence.activities[0].state || 'N/A',
+                        },
+                        'ACTIVITY': {
+                            'TYPE': user.presence.activities[1].type || 'N/A',
+                            'NAME': user.presence.activities[1].name || 'N/A',
+                            'STATE': user.presence.activities[1].state || 'N/A',
+                            'DETAILS': user.presence.activities[1].details || 'N/A',
+                            'ASSETS': {
+                                'SMALLIMAGEURL': user.presence.activities[1].assets.smallImageURL({ size: 2048, format: 'jpg' }) || 'https://icons.veryicon.com/png/o/miscellaneous/unicons/n-a.png',
+                                'SMALLTEXT': user.presence.activities[1].assets.smallText,
+                                'LARGEIMAGEURL': user.presence.activities[1].assets.largeImageURL({ size: 2048, format: 'jpg' }) || 'https://icons.veryicon.com/png/o/miscellaneous/unicons/n-a.png',
+                                'LARGETEXT': user.presence.activities[1].assets.largeText,
+                            }
+                        },
+                    }
+                } else {
+                    API.clear()
+                    var API_VALUE = {
+                        'INFO': {
+                            'NAME': user.username,
+                            'STATUS': user.presence.status,
+                            'AVATAR': user.displayAvatarURL({ size: 2048, format: 'jpg' }),
+                            'CUSTOM_STATUS': user.presence.activities[0].state || 'N/A',
+                        }
+                    }
+                }
+            } else {
+                API.clear()
+                var API_VALUE = {
+                    'INFO': {
+                        'NAME': user.username,
+                        'STATUS': user.presence.status || 'N/A',
+                        'AVATAR': user.displayAvatarURL({ size: 2048, format: 'jpg' }),
+                        'CUSTOM_STATUS': user.presence.activities[0].state || 'N/A',
+                    },
+                    'ACTIVITY': {
+                        'TYPE': user.presence.activities[0].type || 'N/A',
+                        'NAME': user.presence.activities[0].name || 'N/A',
+                        'STATE': user.presence.activities[0].state || 'N/A',
+                        'DETAILS': user.presence.activities[0].details || 'N/A',
+                        'ASSETS': {
+                            'SMALLIMAGEURL': user.presence.activities[0].assets.smallImageURL({ size: 2048, format: 'jpg' }) || 'https://icons.veryicon.com/png/o/miscellaneous/unicons/n-a.png',
+                            'SMALLTEXT': user.presence.activities[0].assets.smallText,
+                            'LARGEIMAGEURL': user.presence.activities[0].assets.largeImageURL({ size: 2048, format: 'jpg' }) || 'https://icons.veryicon.com/png/o/miscellaneous/unicons/n-a.png',
+                            'LARGETEXT': user.presence.activities[0].assets.largeText,
+                        }
+                    },
+                }
+            }
+        } else {
+            API.clear()
+            var API_VALUE = {
+                'INFO': {
+                    'NAME': user.username,
+                    'STATUS': user.presence.status || 'N/A',
+                    'AVATAR': user.displayAvatarURL({ size: 2048, format: 'jpg' }),
+                    'CUSTOM_STATUS': 'N/A',
+                }
+            }
+        }
+        API.set('USER', API_VALUE)
+        console.log('API PATH DATA UPDATED');
+
+    }, 6000);
+})
 
 router.get("/", (req, res) => {
     const config = db.all();
-
+    const DATA = API.all()
     res.render("index", {
         config,
+        DATA,
         layout: "./"
     });
 });
