b35f53dcdd464b95691bc8163c87cc98f86b3d55
hadiazt <hadiazari30@gmail.com>
2024-04-28 15:11:13 +0330

    Refactor GitRepos component to sort and filter popular open-source projects

--- src/components/HomeComponents/GitRepos.tsx
+++ src/components/HomeComponents/GitRepos.tsx
@@ -14,18 +14,24 @@ export const GitRepos = () => {
       axios
         .get("https://api.github.com/users/hadiazt/repos")
         .then(({ data }) => data)
-        .catch((error) => console.error("Error fetching :", error))
+        .catch((error) => console.error("Error fetching:", error))
   );
 
-  const TOPRepos = data?.filter(
-    (repo: { stargazers_count: number; forks_count: number }) =>
-      repo.stargazers_count >= 5 || repo.forks_count >= 5
-  );
+  const TOPRepos = data
+    ?.filter(
+      (repo: { stargazers_count: number; forks_count: number }) =>
+        repo.stargazers_count >= 5 || repo.forks_count >= 5
+    )
+    .sort((repoA, repoB) => {
+      const repoAScore = repoA.stargazers_count + repoA.forks_count;
+      const repoBScore = repoB.stargazers_count + repoB.forks_count;
+      return repoBScore - repoAScore;
+    });
 
   return (
     <>
       <div className="px-2 md:px-8 py-4 text-lg font-bold text-Snow font-IR_Sans">
-        پروژه های متن باز پرطرفدار
+        پروژه های متن باز پرطرفدار ({TOPRepos?.length})
       </div>
       <div className="overflow-x-auto w-full grid  justify-items-center grid-flow-col gap-4 px-2 md:px-8 pt-2 pb-4">
         {isLoading ? (
