d89a9466487c789a99c8ab050ff83f39abe9b660
hadiazt <hadiazari30@gmail.com>
2024-04-28 15:11:52 +0330

    Refactor Home-Page component structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,13 @@ import { MyExpertise } from "../components/HomeComponents/Expertise/MyExpertise"
 
 export default () => {
   return (
-    <div className="Home-Page -z-10" dir="rtl">
-      <Banner />
-      <MyExpertise />
-      <GitRepos />
-      <Footer />
+    <div className="Home-Page -z-10 " dir="rtl">
+      <div className="rounded-3xl overflow-hidden">
+        <Banner />
+        <MyExpertise />
+        <GitRepos />
+        <Footer />
+      </div>
     </div>
   );
 };
