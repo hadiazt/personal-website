b01c708c6ece454c1453d3df4e5a8906fe057561
hadiazt <hadiazari30@gmail.com>
2024-04-28 08:48:23 +0330

    Add configuration files and dependencies for Next.js and TailwindCSS

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        Green: "#1fdf64",
+        Black: "#000",
+        DeepNightBlack: "#121212",
+        MidNightBlack: "#181818",
+        EveningBlack: "#1a1a1a",
+        DarkGray: "#282828",
+        SlateGray: "#404040",
+        LightGray: "#959595",
+        SilverGray: "#B3B3B3",
+        Snow: "#ffffff",
+      },
+      fontFamily: {
+        "cascadia-normal": ["cascadia-normal"],
+        circular: ["circular-normal", "sans-serif"],
+        "circular-light": ["circular-light", "sans-serif"],
+        "circular-normal": ["circular-normal", "sans-serif"],
+        "circular-medium": ["circular-medium", "sans-serif"],
+        "circular-bold": ["circular-bold", "sans-serif"],
+      },
+    },
+  },
+} satisfies Config;
