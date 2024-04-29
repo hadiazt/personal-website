ff05dc8469446dbb0f1295c365ff64ff409aa64d
hadiazt <hadiazari30@gmail.com>
2024-04-29 14:59:23 +0330

    Remove exec_mode and instances from ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,6 @@ module.exports = {
   apps: [
     {
       name: "Personal Website",
-      exec_mode: "cluster",
-      instances: 1,
       script: "node_modules/next/dist/bin/next",
     },
   ],
