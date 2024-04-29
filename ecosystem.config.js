8a2daf530317afbf9595edd515f5af882eba7aba
hadiazt <hadiazari30@gmail.com>
2024-04-29 15:07:04 +0330

    Update start command in ecosystem.config.js and package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,8 @@ module.exports = {
     {
       name: "Personal Website",
       script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      exec_interpreter: "node",
     },
   ],
 };
