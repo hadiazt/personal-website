11d483123d56aa72af6408a46575b425fd59de08
hadiazt <hadiazari30@gmail.com>
2024-05-03 18:39:10 +0330

    Refactor project card component and update API endpoint

--- src/components/HomeComponents/Projects/MyProjects.tsx
+++ src/components/HomeComponents/Projects/MyProjects.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 import { ProjectsCard } from "./ProjectsCard";
-import { ParagraphSkeleton } from "../../Common/ParagraphSkeleton";
+import { ImageAndParagraphSkeleton } from "../../Common/ImageAndParagraphSkeleton";
 
 export const MyProjects = () => {
-  const { isLoading, error, data } = useQuery("expertise", () =>
+  const { isLoading, error, data } = useQuery("projects", () =>
     axios
-      .get("api/expertise")
+      .get("api/projects")
       .then(({ data }) => data)
       .catch((error) => console.error("Error fetching :", error))
   );
@@ -14,12 +14,12 @@ export const MyProjects = () => {
   return (
     <>
       <div className="px-2 md:px-8 py-4 text-lg font-bold text-Snow font-IR_Sans">
-        توانایی های من
+        پروژه های من
       </div>
       <div className="grid justify-items-center grid-flow-row md:grid-cols-2 lg:grid-cols-3 grid-rows-auto gap-4 px-2 md:px-8">
         {isLoading
-          ? [1, 2, 3, 4, 5, 6].map((_, index) => (
-              <ParagraphSkeleton className="space-y-2 p-8" />
+          ? [1, 2, 3].map((_, index) => (
+              <ImageAndParagraphSkeleton className="space-y-2 p-8" />
             ))
           : data?.map((item: any, index: number) => (
               <ProjectsCard key={index} data={item} />
